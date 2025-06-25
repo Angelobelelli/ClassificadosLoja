20961bf0c2a738a99ebdd662169670f0c08bfc3e
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-06-25 12:02:51 -0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-import {useState} from "react";
 import "./App.css";
 import {Outlet, Link} from "react-router-dom";
 
-
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<Outlet />
 			{/* Outlet é onde os componentes filhos das rotas são renderizados */}
-		
 		</>
 	);
 }
